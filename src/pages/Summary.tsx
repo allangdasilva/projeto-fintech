2c978edcf43137fc7b621e6f487f8e586861521d
Allan Silva <allandasilva33@outlook.com>
2025-04-26 00:02:58 -0300

    Resumo das vendas completo

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -2,8 +2,55 @@ import { useData } from "../context/DataContext";
 
 const Summary = () => {
   const { data } = useData();
-  console.log(data);
-  return <div>Summary</div>;
+  if (data === null) return null;
+  return (
+    <>
+      <section>
+        <div className="summary flex mb">
+          <div className="box">
+            <h2>Vendas</h2>
+            <span>
+              {data
+                // Filtro vai retornar apenas os itens/obj que não tiver o status falha
+                .filter((i) => i.status !== "falha")
+                .reduce((acc, item) => acc + item.preco, 0)
+                .toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+            </span>
+          </div>
+          <div className="box">
+            <h2>Recebido</h2>
+            <span>
+              {data
+                // Filtro vai retornar apenas os itens/obj que tiver o status igual
+                // a 'pago', e fazer a somar normal do preco
+                .filter((i) => i.status === "pago")
+                .reduce((acc, item) => acc + item.preco, 0)
+                .toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+            </span>
+          </div>
+          <div className="box">
+            <h2>Vendas</h2>
+            <span>
+              {data
+                .filter((i) => i.status === "processando")
+                .reduce((acc, item) => acc + item.preco, 0)
+                .toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+            </span>
+          </div>
+        </div>
+        <div className="box">Gráficos</div>
+      </section>
+    </>
+  );
 };
 
 export default Summary;
