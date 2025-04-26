414b353b48115332b1a204fa44cbaaa39c945358
Allan Silva <allandasilva33@outlook.com>
2025-04-26 01:14:54 -0300

    Rotas para página de venda e resumo definidas

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Sidenav from "./components/Sidenav";
 import { DataContextProvider } from "./context/DataContext";
 import Sales from "./pages/Sales";
 import Summary from "./pages/Summary";
+import Sale from "./pages/Sale";
 
 function App() {
   return (
     <>
-      <DataContextProvider>
-        <div className="container">
-          <Sidenav />
-          <main>
-            <Header />
-            <Summary />
-            <Sales />
-          </main>
-        </div>
-      </DataContextProvider>
+      <BrowserRouter>
+        <DataContextProvider>
+          <div className="container">
+            <Sidenav />
+            <main>
+              <Header />
+              <Routes>
+                <Route path="/" element={<Summary />} />
+                <Route path="/sales" element={<Sales />} />
+                <Route path="/sales/:id" element={<Sale />} />
+              </Routes>
+            </main>
+          </div>
+        </DataContextProvider>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import DateRange from "./DateRange";
 import Months from "./Months";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
   const [title, setTitle] = React.useState("Resumo");
+  // useLocation é um hook do Router que verifica em qual página estou no momento
+  const location = useLocation();
+  React.useEffect(() => {
+    if (location.pathname === "/") {
+      setTitle("Resumo");
+      document.title = "Fintech | Resumo";
+    } else if (location.pathname === "/sales") {
+      setTitle("Vendas");
+      document.title = "Fintech | Vendas";
+    }
+  }, [location]);
   return (
     <>
       <header className="mb">
